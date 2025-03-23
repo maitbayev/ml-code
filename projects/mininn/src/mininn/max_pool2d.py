7268928c94de0c667a4d5302a8b36b9b66f4cf0a
Madiyar Aitbayev <madiyar92@gmail.com>
2025-03-23 00:25:46 +0000

    backward for max_pool2d

--- projects/mininn/src/mininn/max_pool2d.py
+++ projects/mininn/src/mininn/max_pool2d.py
@@ -13,12 +13,14 @@ class MaxPool2D(Module):
         self.kernel_size = kernel_size
         self.stride = stride or kernel_size
         self.padding = padding
+        self._padded_input = np.ndarray([])
 
     def forward(self, input: np.ndarray) -> np.ndarray:
         k = self.kernel_size
         if self.padding > 0:
             p = (self.padding, self.padding)
             input = np.pad(input, ((0, 0), (0, 0), p, p), constant_values=-np.inf)
+        self._padded_input = input
         # (B, C, H, W)
         batches, c, h, w = input.shape
         h_out, w_out = (h - k) // self.stride + 1, (w - k) // self.stride + 1
@@ -30,4 +32,21 @@ class MaxPool2D(Module):
         return output
 
     def backward(self, gradients: np.ndarray) -> np.ndarray:
-        return np.ndarray([])
+        k = self.kernel_size
+        batches, c, h, w = self._padded_input.shape
+        grad_input = np.zeros_like(self._padded_input)
+        for i in range(0, h - k + 1, self.stride):
+            for j in range(0, w - k + 1, self.stride):
+                window = self._padded_input[:, :, i : i + k, j : j + k].reshape(
+                    (batches, c, -1)
+                )
+                index = np.argmax(window, axis=2)
+                for b in range(batches):
+                    for ci in range(c):
+                        grad_input[
+                            b, ci, index[b][ci] // k + i, index[b][ci] % k + j
+                        ] += gradients[b, ci, i // self.stride, j // self.stride]
+        if self.padding > 0:
+            p = self.padding
+            grad_input = grad_input[:, :, p:-p, p:-p]
+        return grad_input
--- projects/mininn/tests/max_pool2d_test.py
+++ projects/mininn/tests/max_pool2d_test.py
@@ -19,3 +19,23 @@ def test_forward():
             inp_torch, k, stride, padding=padding
         )
         assert max_pool2d(inp) == approx(max_pool2d_torch.numpy(force=True))
+
+
+def test_backward():
+    for _ in range(100):
+        k = random.randint(1, 3) * 2 + 1
+        stride = random.randint(2, 5)
+        padding = random.randint(0, k // 2)
+        inp = np.random.rand(4, 5, 10, 10)
+        max_pool2d = mininn.MaxPool2D(k, stride, padding)
+        out = max_pool2d(inp)
+        grad: np.ndarray = np.random.rand(*out.shape)  # type: ignore
+
+        inp_torch = torch.tensor(inp, requires_grad=True)
+        max_pool2d_torch = torch.nn.functional.max_pool2d(
+            inp_torch, k, stride, padding=padding
+        )
+        max_pool2d_torch.backward(torch.tensor(grad))
+
+        assert inp_torch.grad is not None
+        assert max_pool2d.backward(grad) == approx(inp_torch.grad.numpy(force=True))
